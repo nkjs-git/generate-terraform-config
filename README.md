b0367a2649b1ff115428f63cb62c054c5261b125
Jain S <naveenkumar.jains@siemens-healthineers.com>
2024-07-10 22:32:49 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+terraform init
+
+
+terraform plan -generate-config-out="kvGenerated.tf"
+
+
+Ref: https://www.youtube.com/watch?v=SShHumn5Grc
+
+
+You can also use tird party tools like aztfexport which is still in preview
---
+++ resources.tf
@@ -0,0 +1,6 @@
+import {
+    id = "/subscriptions/64c6b93a-7f94-4fb0-acce-d6e8015ce031/resourceGroups/nkjs-kv-rg/providers/Microsoft.KeyVault/vaults/nkjs-kv"
+    to = azurerm_key_vault.kv-imported
+}
+
+# resource "azurerm_key_vault" "kv-imported" {}
